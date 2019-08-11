ff39e99fc18b93d0e721fbac4e70d660337a75f2
Damian Krzeminski <pirxpilot@furkot.com>
2019-08-11 17:59:25 -0600

    fix exception when key is missing
    Bromba does not know that key is missing - it returns Buffers filled
    with 0 for values that were never initialized
    
    this fix takes care of the case when missing key is located in the
    sector that was never initialized

--- test/bromba.js
+++ test/bromba.js
@@ -49,8 +49,32 @@ test('getMany', function (t) {
   });
 });
 
+test('getMany with missing key', function (t) {
+
+  const b = bromba(name, { vlen: 2 });
+
+  const keys = [
+    [ 0, 0, 1, 2, 1 ],
+    [ 0, 0, 1, 2, 0 ], // missing key - existing sector
+    [ 0, 0, 1, 2, 3 ],
+    [ 0, 9, 1, 2, 0 ], // missing key - sector does not exist
+  ].map(Buffer.from);
+
+  b.getMany(keys, function(err, vs) {
+    t.error(err);
+
+    t.same(vs[0], values[0], `same value 0`);
+    t.same(vs[1], Buffer.alloc(2));
+    t.same(vs[2], values[2], `same value 2`);
+    t.same(vs[3], Buffer.alloc(2));
+
+    b.close(t.end);
+  });
+});
 
 test('cleanup', function(t) {
   removeCallback();
   t.end();
 });
+
+
